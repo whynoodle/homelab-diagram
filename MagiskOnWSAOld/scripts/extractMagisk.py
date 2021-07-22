6db5d5ec7a68ae9e171b6335db5f4516264812a9
whynoodle <119311098+whynoodle@users.noreply.github.com>
2021-07-22 13:32:45 +0000

    Create COPYRIGHT

--- MagiskOnWSAOld/scripts/extractMagisk.py
+++ MagiskOnWSAOld/scripts/extractMagisk.py
@@ -8,4 +8,8 @@
 # License, or (at your option) any later version.
 #
 # MagiskOnWSALocal is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the im
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU 
