1b731cc31ab4e85961f401b8659f84cc6c64be58
whynoodle <119311098+whynoodle@users.noreply.github.com>
2021-08-12 07:14:38 +0000

    Fixes formatting

--- MagiskOnWSAOld/scripts/fixGappsProp.py
+++ MagiskOnWSAOld/scripts/fixGappsProp.py
@@ -1,1 +1,7 @@
-#!/usr/bin/pyth
+#!/usr/bin/python3
+#
+# This file is part of MagiskOnWSALocal.
+#
+# MagiskOnWSALocal is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by 
