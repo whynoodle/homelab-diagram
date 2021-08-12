739d1ff5e1a8d473bcb65065a9a4511e7dde059f
whynoodle <119311098+whynoodle@users.noreply.github.com>
2021-08-12 22:57:21 +0000

    up

--- MagiskOnWSAOld/scripts/fixGappsProp.py
+++ MagiskOnWSAOld/scripts/fixGappsProp.py
@@ -4,4 +4,7 @@
 #
 # MagiskOnWSALocal is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as
-# published by 
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# MagiskOnWSALocal is distributed in the hope that it will be
