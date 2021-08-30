c340ad911f49cacca20f0bc17d56fc3c94c73342
whynoodle <119311098+whynoodle@users.noreply.github.com>
2021-08-30 20:32:28 +0000

    concurrency

--- MagiskOnWSAOld/scripts/generateGappsLink.py
+++ MagiskOnWSAOld/scripts/generateGappsLink.py
@@ -10,4 +10,7 @@
 # MagiskOnWSALocal is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Affero General Public License
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with MagiskOnWSALocal.  If not, see <http
