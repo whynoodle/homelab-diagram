b2925e85520e79e96546c05bc0b86a46be3fb53b
whynoodle <119311098+whynoodle@users.noreply.github.com>
2021-09-12 19:27:22 +0000

    cifs: on CONFIG_HIGHMEM machines, limit the rsize/wsize to the kmap space

--- MagiskOnWSAOld/scripts/generateMagiskLink.py
+++ MagiskOnWSAOld/scripts/generateMagiskLink.py
@@ -3,4 +3,8 @@
 # This file is part of MagiskOnWSALocal.
 #
 # MagiskOnWSALocal is free software: you can redistribute it and/or modify
-# it under the terms of 
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# MagiskOnWSALoc
